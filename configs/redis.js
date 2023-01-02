6faf51a4d471377d1a6c194df681ffb4f1c8558e
John Garland <johnpg82@gmail.com>
2023-01-02 13:22:46 -0700

    Update redis.js

--- configs/redis.js
+++ configs/redis.js
@@ -1,16 +1,20 @@
 const redisapi = require('redis');
 
+
+const redisUrl = process.env.REDIS_TLS_URL ? process.env.REDIS_TLS_URL : process.env.REDIS_URL;
+const redisDefaults = {
+  tls: {
+  // Heroku uses self-signed certificate, which will cause error in connection, unless check is disabled
+  rejectUnauthorized: false,
+  },
+};
+//const defaultClient = redis.createClient(redisUrl, redisDefaults);
+
 // Socket required for node redis <-> docker-compose connection
 
 //const Redis = createClient({ socket: { host: 'redis', port: 6379 } });
 
-const Redis = redisapi.createClient({
-    url: process.env.REDIS_TLS_URL,
-    socket: {
-      tls: true,
-      rejectUnauthorized: false
-    }
-  });
+const Redis = redisapi.createClient(redisUrl,redisDefaults);
 
 
 module.exports = Redis;
